7052d30de85f4c0d37853da94b5a21d953403c3c
stirlingx001 <yue@polyhedra.network>
2024-12-16 10:33:56 +0800

    test

--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import (
 	"bytes"
 	"context"
 	"encoding/binary"
-	"encoding/hex"
+	"errors"
 	"fmt"
-	"github.com/cometbft/cometbft/libs/os"
 	cmttypes "github.com/cometbft/cometbft/types"
 	"github.com/oasisprotocol/oasis-core/go/common/cbor"
 	"github.com/oasisprotocol/oasis-core/go/common/crypto/hash"
+	"github.com/oasisprotocol/oasis-core/go/storage/mkvs/node"
 	mkvsNode "github.com/oasisprotocol/oasis-core/go/storage/mkvs/node"
 	"github.com/oasisprotocol/oasis-core/go/storage/mkvs/syncer"
 	"github.com/oasisprotocol/oasis-sdk/client-sdk/go/config"
@@ -54,7 +54,7 @@ func main() {
 	cobra.CheckErr(err)
 	fmt.Printf("Consensus state root hash: %x\n", meta.Header.AppHash.Bytes())
 
-	os.WriteFile("1.txt", []byte(hex.EncodeToString(cb.Meta[:])), 0766)
+	//os.WriteFile("1.txt", []byte(hex.EncodeToString(cb.Meta[:])), 0766)
 
 	d := cbor.Marshal(meta)
 
@@ -94,6 +94,7 @@ func main() {
 	// Step 4: Extract the two root hashes from the verified proof. First is runtime state root
 	//         at round blockNum, second is runtime I/O root at round blockNum.
 	var verifiedRoots []hash.Hash
+	var pastRootsStorageValue []byte
 	for _, v := range wl {
 		if !bytes.Equal(v.Key, pastRootsStorageKey) {
 			continue
@@ -102,11 +103,21 @@ func main() {
 		err = cbor.Unmarshal(v.Value, &verifiedRoots)
 		fmt.Printf("v.Value: %x\n", v.Value)
 
+		pastRootsStorageValue = v.Value
+
 		cobra.CheckErr(err)
 		break
 	}
 
-	//cbor.Marshal(&verifiedRoots)
+	{
+		var nodes []*NodeData
+		travelProof(ctx, &pr.Proof, 0, func(n *NodeData) {
+			nodes = append(nodes, n)
+		})
+		fmt.Printf("len(nodes): %v\n", len(nodes))
+		leafHash := HashLeafNode(pastRootsStorageKey, pastRootsStorageValue)
+		fmt.Printf("verifyProof: %v\n", verifyProof(nodes, consensusStateRoot.Hash, leafHash))
+	}
 
 	verifiedIORootHash := verifiedRoots[1]
 	fmt.Printf("Runtime height:            %d\n", blockNum)
@@ -119,152 +130,12 @@ func main() {
 	err = txHash.UnmarshalHex(txHashHex)
 	cobra.CheckErr(err)
 
-	txStorageKey := append([]byte{'T'}, txHash[:]...)
-	txStorageKey = append(txStorageKey, byte(0x01))
-
 	rc := conn.Runtime(pt)
-	pr, err = rc.State().SyncGet(ctx, &syncer.GetRequest{
-		Tree: syncer.TreeID{
-			Root: mkvsNode.Root{
-				Namespace: pt.Namespace(),
-				Version:   blockNum,
-				Type:      mkvsNode.RootTypeIO,
-				Hash:      verifiedIORootHash,
-			},
-			Position: verifiedIORootHash,
-		},
-		Key:          txStorageKey,
-		ProofVersion: 1,
-	})
-	cobra.CheckErr(err)
-
-	// Step 6: Verify Merkle proof against the verified I/O root. This proves that the transaction
-	//         is included in a block and also gets the raw transaction itself.
-	wl, err = pv.VerifyProofToWriteLog(ctx, verifiedIORootHash, &pr.Proof)
-	cobra.CheckErr(err)
-
-	// Step 7: Extract the transaction from the verified proof.
-	//var verifiedTx []byte
-	//for _, v := range wl {
-	//	if !bytes.Equal(v.Key, txStorageKey) {
-	//		continue
-	//	}
-	//
-	//	type inputArtifacts struct {
-	//		_ struct{} `cbor:",toarray"` // nolint
-	//
-	//		// Input is the transaction input.
-	//		Input []byte
-	//		// Order is the transaction order within the block.
-	//		Order uint32
-	//	}
-	//	var ia inputArtifacts
-	//	err = cbor.Unmarshal(v.Value, &ia)
-	//	cobra.CheckErr(err)
-	//
-	//	fmt.Printf("inputArtifacts Value: %x\n", v.Value)
-	//
-	//	verifiedTx = ia.Input
-	//	break
-	//}
-
-	// Step 8: Parse SDK-wrapped Ethereum transaction.
-	//var tx types.UnverifiedTransaction
-	//err = cbor.Unmarshal(verifiedTx, &tx)
-	//cobra.CheckErr(err)
-	//
-	//if len(tx.AuthProofs) != 1 || tx.AuthProofs[0].Module != "evm.ethereum.v0" {
-	//	panic("unexpected non-ethereum transaction")
-	//}
-	//
-	//var ethTx ethTypes.Transaction
-	//if err = ethTx.UnmarshalBinary(tx.Body); err != nil {
-	//	panic("unexpected malformed ethereum transaction")
-	//}
-	//
-	//fmt.Printf("Transaction hash:          %s\n", tx.Hash())
-	//fmt.Printf("Eth transaction hash:      %s\n", ethTx.Hash())
-	//
-	//if tx.Hash().String() != txHashHex {
-	//	panic("unexpected SDK-wrapped transaction hash")
-	//}
-	//if ethTx.Hash().String() != txEthHashHex {
-	//	panic("unexpected eth transaction hash")
-	//}
-
-	// Step 9: Query the transaction result from the verified I/O root.
-	//
-	//         The storage key format is: T <tx-hash> 0x02
-	//txStorageKey = append([]byte{'T'}, txHash[:]...)
-	//txStorageKey = append(txStorageKey, byte(0x02))
-	//
-	//pr, err = rc.State().SyncGet(ctx, &syncer.GetRequest{
-	//	Tree: syncer.TreeID{
-	//		Root: mkvsNode.Root{
-	//			Namespace: pt.Namespace(),
-	//			Version:   blockNum,
-	//			Type:      mkvsNode.RootTypeIO,
-	//			Hash:      verifiedIORootHash,
-	//		},
-	//		Position: verifiedIORootHash,
-	//	},
-	//	Key:          txStorageKey,
-	//	ProofVersion: 1,
-	//})
-	//cobra.CheckErr(err)
-
-	// Step 10: Verify Merkle proof against the verified I/O root. This proves that the transaction
-	//          has been executed within a block and also gets its raw result.
-	//wl, err = pv.VerifyProofToWriteLog(ctx, verifiedIORootHash, &pr.Proof)
-	//cobra.CheckErr(err)
-
-	// Step 11: Extract the output from the verified proof.
-	//var verifiedTxResult []byte
-	//for _, v := range wl {
-	//	if !bytes.Equal(v.Key, txStorageKey) {
-	//		continue
-	//	}
-	//
-	//	type outputArtifacts struct {
-	//		_ struct{} `cbor:",toarray"` // nolint
-	//
-	//		// Output is the transaction output.
-	//		Output []byte
-	//	}
-	//	var oa outputArtifacts
-	//	err = cbor.Unmarshal(v.Value, &oa)
-	//	cobra.CheckErr(err)
-	//
-	//	verifiedTxResult = oa.Output
-	//	break
-	//}
-
-	// Step 12: Parse the transaction result.
-	//var txResult types.CallResult
-	//err = cbor.Unmarshal(verifiedTxResult, &txResult)
-	//cobra.CheckErr(err)
-
-	//switch res := txResult; {
-	//case res.Failed != nil:
-	//	fmt.Printf("Runtime height:            %d\n", blockNum)
-	//	fmt.Printf("Transaction status:        failed\n")
-	//	fmt.Printf("            module:        %s\n", res.Failed.Module)
-	//	fmt.Printf("            code:          %d\n", res.Failed.Code)
-	//	fmt.Printf("            message:       %s\n", res.Failed.Message)
-	//case res.Ok != nil:
-	//	fmt.Printf("Transaction status:        ok\n")
-	//	fmt.Printf("            data:          0x%X\n", res.Ok)
-	//case res.Unknown != nil:
-	//	fmt.Printf("Transaction status:        unknown\n")
-	//	fmt.Printf("            data:          0x%X\n", res.Unknown)
-	//default:
-	//	panic("unexpected result kind")
-	//}
 
 	// Step 13: Query the events emitted by the transaction from the verified I/O root.
 	//
 	//         The storage key format is: E <tag> <tx-hash>
-	txStorageKey = append([]byte{'E'}, []byte("evm\x00\x00\x00\x01")...)
+	txStorageKey := append([]byte{'E'}, []byte("evm\x00\x00\x00\x01")...)
 	txStorageKey = append(txStorageKey, txHash[:]...)
 
 	pr, err = rc.State().SyncGet(ctx, &syncer.GetRequest{
@@ -287,6 +158,7 @@ func main() {
 	wl, err = pv.VerifyProofToWriteLog(ctx, verifiedIORootHash, &pr.Proof)
 	cobra.CheckErr(err)
 
+	var logValue []byte
 	// Step 15: Extract the output from the verified proof.
 	var logs []*evm.Event
 	for _, v := range wl {
@@ -296,8 +168,22 @@ func main() {
 
 		err = cbor.Unmarshal(v.Value, &logs)
 		cobra.CheckErr(err)
+
+		logValue = v.Value
+
 		fmt.Printf("log value: %x\n", v.Value)
+
 		break
+	}
+
+	{
+		var nodes []*NodeData
+		travelProof(ctx, &pr.Proof, 0, func(n *NodeData) {
+			nodes = append(nodes, n)
+		})
+		fmt.Printf("len(nodes): %v\n", len(nodes))
+		leafHash := HashLeafNode(txStorageKey, logValue)
+		fmt.Printf("verifyProof: %v\n", verifyProof(nodes, verifiedIORootHash, leafHash))
 	}
 
 	fmt.Printf("EVM events:\n")
@@ -309,4 +195,172 @@ func main() {
 		}
 		fmt.Printf("  Data: 0x%X\n", log.Data)
 	}
+}
+
+type NodeData struct {
+	Hash hash.Hash
+	// Label is the label on the incoming edge.
+	Label node.Key
+	// LabelBitLength is the length of the label in bits.
+	LabelBitLength node.Depth
+
+	// LeafNode is for the key ending at this depth.
+	LeafNodeHash hash.Hash
+	//LeafNodeKey   node.Key
+	//LeafNodeValue []byte
+
+	LeftNodeHash  hash.Hash
+	RightNodeHash hash.Hash
+}
+
+type Visitor func(n *NodeData)
+
+func travelProof(ctx context.Context, proof *syncer.Proof, idx int, visit Visitor) (int, *node.Pointer, error) {
+	if ctx.Err() != nil {
+		return -1, nil, ctx.Err()
+	}
+	if idx >= len(proof.Entries) {
+		return -1, nil, errors.New("verifier: malformed proof")
+	}
+
+	entry := proof.Entries[idx]
+	if entry == nil {
+		return idx + 1, nil, nil
+	}
+	if len(entry) == 0 {
+		return -1, nil, errors.New("verifier: malformed proof")
+	}
+
+	switch entry[0] {
+	case 1:
+		// Full node.
+		n, err := node.UnmarshalBinary(entry[1:])
+		if err != nil {
+			return -1, nil, err
+		}
+
+		// For internal nodes, also decode children.
+		pos := idx + 1
+		if nd, ok := n.(*node.InternalNode); ok {
+			pos, nd.LeafNode, err = travelProof(ctx, proof, pos, visit)
+			if err != nil {
+				return -1, nil, err
+			}
+
+			// Left.
+			pos, nd.Left, err = travelProof(ctx, proof, pos, visit)
+			if err != nil {
+				return -1, nil, err
+			}
+			// Right.
+			pos, nd.Right, err = travelProof(ctx, proof, pos, visit)
+			if err != nil {
+				return -1, nil, err
+			}
+
+			// Recompute hash as hashes were not recomputed for compact encoding.
+			nd.UpdateHash()
+
+			n := &NodeData{
+				Hash:           nd.Hash,
+				Label:          nd.Label,
+				LabelBitLength: nd.LabelBitLength,
+				LeafNodeHash:   nd.LeafNode.GetHash(),
+				LeftNodeHash:   nd.Left.GetHash(),
+				RightNodeHash:  nd.Right.GetHash(),
+			}
+			visit(n)
+		}
+
+		ptr := &node.Pointer{Clean: true, Hash: n.GetHash(), Node: n}
+
+		return pos, ptr, nil
+	case 2:
+		// Hash of a node.
+		var h hash.Hash
+		if err := h.UnmarshalBinary(entry[1:]); err != nil {
+			return -1, nil, err
+		}
+
+		//visit(&NodeData{Hash: h})
+
+		return idx + 1, &node.Pointer{Clean: true, Hash: h}, nil
+	default:
+		return -1, nil, fmt.Errorf("verifier: unexpected entry in proof (%x)", entry[0])
+	}
+}
+
+func verifyProof(nodes []*NodeData, rootHash, leafHash hash.Hash) bool {
+	if len(nodes) == 1 {
+		if bytes.Equal(nodes[0].Hash[:], rootHash[:]) {
+			return true
+		}
+		return false
+	}
+
+	foundLeafNode := false
+	for _, n := range nodes {
+		if !verifyNode(n) {
+			return false
+		}
+		if bytes.Equal(n.LeafNodeHash[:], leafHash[:]) ||
+			bytes.Equal(n.LeftNodeHash[:], leafHash[:]) ||
+			bytes.Equal(n.RightNodeHash[:], leafHash[:]) {
+			foundLeafNode = true
+		}
+	}
+	if !foundLeafNode {
+		return false
+	}
+	for i := 0; i < len(nodes); i++ {
+		count := findParentCount(nodes[i], nodes)
+		if count > 1 {
+			return false
+		}
+		if count == 0 {
+			if !bytes.Equal(nodes[i].Hash[:], rootHash[:]) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func findParentCount(n *NodeData, nodes []*NodeData) int {
+	count := 0
+	for i := 0; i < len(nodes); i++ {
+		if bytes.Equal(nodes[i].LeftNodeHash[:], n.Hash[:]) || bytes.Equal(nodes[i].RightNodeHash[:], n.Hash[:]) {
+			count++
+		}
+	}
+	return count
+}
+
+func HashLeafNode(key, value []byte) hash.Hash {
+	var keyLen, valueLen [4]byte
+	binary.LittleEndian.PutUint32(keyLen[:], uint32(len(key)))
+	binary.LittleEndian.PutUint32(valueLen[:], uint32(len(value)))
+
+	var h hash.Hash
+	h.FromBytes([]byte{node.PrefixLeafNode}, keyLen[:], key, valueLen[:], value)
+	return h
+}
+
+func verifyNode(n *NodeData) bool {
+
+	leafNodeHash := n.LeafNodeHash
+	leftHash := n.LeftNodeHash
+	rightHash := n.RightNodeHash
+	labelBitLength := n.LabelBitLength.MarshalBinary()
+
+	var h hash.Hash
+	h.FromBytes(
+		[]byte{node.PrefixInternalNode},
+		labelBitLength,
+		n.Label[:],
+		leafNodeHash[:],
+		leftHash[:],
+		rightHash[:],
+	)
+	return bytes.Equal(h[:], n.Hash[:])
 }
